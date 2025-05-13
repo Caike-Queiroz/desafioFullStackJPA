bcfcfa5c4b64040b17b8b1b6692bc151e769e759
Caike884 <caikerodriguesqueiroz@gmail.com>
2025-05-13 20:02:35 -0300

    First commit

---
+++ backend/DesafioFullStackJPA/src/main/java/br/com/alura/caike/DesafioFullStackJPA/DesafioFullStackJpaApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.caike.DesafioFullStackJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioFullStackJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioFullStackJpaApplication.class, args);
+	}
+
+}
---
+++ backend/DesafioFullStackJPA/src/test/java/br/com/alura/caike/DesafioFullStackJPA/DesafioFullStackJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.caike.DesafioFullStackJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioFullStackJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
